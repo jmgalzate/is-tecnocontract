24bea263e6d6ef845838e4ac1d9ece2022f6a977
Juan Miguel <jmgalzate@outlook.com>
2022-09-08 16:59:32 -0500

    index

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php
 
-#The index is requiring te bootstrap, because it's the bootstrap file that connect with the Core file for redirection of Controllers - Models - Views
+/**
+ * The index is requiring te bootstrap, because it's the bootstrap file that connect with the Core file for redirection of Controllers - Models - Views
+ */
+
 require_once 'app/bootstrap.php';
 
 // Init Core library
